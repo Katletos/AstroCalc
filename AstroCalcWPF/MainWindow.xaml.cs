e12cb5622e11f58ed206003d68117983c34d1af3
Mikita <40485432+Katletos@users.noreply.github.com>
2020-06-27 21:56:15 +0300

    fix

--- AstroCalcWPF/MainWindow.xaml.cs
+++ AstroCalcWPF/MainWindow.xaml.cs
@@ -22,20 +22,14 @@ namespace AstroCalcWPF
 
         private void Stellarium_Checked(object sender, RoutedEventArgs e)
         {
-            // FrameWithinGrid.Source = new Uri("https://stellarium-web.org/", UriKind.Absolute);
-
-            wbSample.Navigate("https://stellarium-web.org/");
+            FrameWithinGrid.Navigate(new Uri("Stellarium.xaml",
+            UriKind.RelativeOrAbsolute));
         }
 
         private void Teleskopes_Checked(object sender, RoutedEventArgs e)
         {
             FrameWithinGrid.Navigate(new Uri("Telescopes.xaml",
             UriKind.RelativeOrAbsolute));
-        }
-
-        private void wbSample_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
-        {
-
         }
     }
 }
---
+++ AstroCalcWPF/Stellarium.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Forms;
+using WebBrowser = System.Windows.Forms.WebBrowser;
+
+namespace AstroCalcWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Stellarium.xaml
+    /// </summary>
+    public partial class Stellarium : Page
+    {
+        public Stellarium()
+        {
+            InitializeComponent();
+        }
+    }
+}
