597e6a4a83a671357f3e63b7ebe76fa9691b5601
Mikita <40485432+Katletos@users.noreply.github.com>
2020-07-06 20:00:32 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+
+
+namespace AstroCalcWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Satellite_Checked(object sender, RoutedEventArgs e)
+        {
+            FrameWithinGrid.Navigate(new Uri("Satellite.xaml",
+            UriKind.RelativeOrAbsolute));
+        }
+
+        private void Stellarium_Checked(object sender, RoutedEventArgs e)
+        {
+            FrameWithinGrid.Navigate(new Uri("Stellarium.xaml",
+            UriKind.RelativeOrAbsolute));
+        }
+
+        private void Teleskopes_Checked(object sender, RoutedEventArgs e)
+        {
+            FrameWithinGrid.Navigate(new Uri("Telescopes.xaml",
+            UriKind.RelativeOrAbsolute));
+        }
+    }
+}
---
+++ Telescopes.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AstroCalcWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Telescopes.xaml
+    /// </summary>
+    public partial class Telescopes : Page
+    {
+        public Telescopes()
+        {
+            InitializeComponent();
+        }
+
+        public int DS_Count(string s)
+        {
+            string substr = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0].ToString();
+            int count = (s.Length - s.Replace(substr, "").Length) / substr.Length;
+            return count;
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !(char.IsDigit(e.Text, 0) || ((e.Text == System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0].ToString()) && (DS_Count(((TextBox)sender).Text) < 1)));
+        }
+    }
+}
